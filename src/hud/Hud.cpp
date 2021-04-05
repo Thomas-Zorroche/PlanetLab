98ffe4f397962e83fbd00a11b880eb8a88fa95dd
Thomas Zorroche <thomas.zorroche@gmail.com>
2021-04-05 23:42:23 +0200

    [ui] add dockspace

--- src/hud/Hud.cpp
+++ src/hud/Hud.cpp
@@ -49,66 +49,116 @@ void Hud::init(GLFWwindow* window, float width, float height)
 
 void Hud::draw(const std::shared_ptr<Camera>& camera) const
 {
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+
     //New Frame
     ImGui_ImplOpenGL3_NewFrame();
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
+     
+    static bool dockspaceOpen = true;
+    static bool opt_fullscreen = true;
+    static bool opt_padding = false;
+    static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
 
-    ImGuiWindowFlags window_flags = 0;
-    window_flags |= ImGuiWindowFlags_NoCollapse;
-    window_flags |= ImGuiWindowFlags_NoTitleBar;
-    window_flags |= ImGuiWindowFlags_MenuBar;
-
-    ImGui::SetNextWindowPos(ImVec2(0, 0));
-    ImGui::SetNextWindowSize(ImVec2(_WIDTH, _HEIGHT));
-    // Main Window
-    if (ImGui::Begin("Procedural Planets", false, window_flags))
+    // We are using the ImGuiWindowFlags_NoDocking flag to make the parent window not dockable into,
+    // because it would be confusing to have two docking targets within each others.
+    ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+    if (opt_fullscreen)
     {
-        // Menu Bar
-        if (ImGui::BeginMenuBar())
-        {
-            if (ImGui::BeginMenu("File"))
-            {
-                ImGui::MenuItem("New");
-                ImGui::MenuItem("Save");
-                ImGui::MenuItem("Save As");
-                ImGui::EndMenu();
-            }
-            if (ImGui::BeginMenu("Tools"))
-            {
-                ImGui::MenuItem("Noise Settings");
-                ImGui::EndMenu();
-            }
-            ImGui::EndMenuBar();
-        }
-
-
-        if (ImGui::BeginChild("Renderer", ImVec2(_viewportWidth, _HEIGHT)))
-        {
-            ImVec2 wsize = ImGui::GetWindowSize();
-            ImGui::Image((ImTextureID)_fboViewport, wsize, ImVec2(0, 1), ImVec2(1, 0));
-        }
-        ImGui::EndChild();
-
-        ImGui::SameLine();
-
-        // Settings
-        if (ImGui::BeginChild("Procedural Planets Settings"))
-        {
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            ImGui::Checkbox("Wireframe Mode", &_wireframeMode);
-            ImGui::SliderInt("Resolution", &_resolution, 4, 128);
-            ImGui::ColorEdit3("Planet Color", (float*)&_planetColor);
-            ImGui::SliderFloat("Size", &_planetRadius, 0.2f, 4.0f);
-            ImGui::SliderFloat("Strength", &_noiseStrength, 0.0f, 2.0f);
-            ImGui::SliderFloat("Roughness", &_noiseRoughness, 0.0f, 20.0f);
-            ImGui::SliderFloat3("Center", (float*)&_noiseCenter, -10.0f, 10.0f);
-        }
-        ImGui::EndChild(); // Settings
-
-
+        const ImGuiViewport* viewport = ImGui::GetMainViewport();
+        ImGui::SetNextWindowPos(viewport->WorkPos);
+        ImGui::SetNextWindowSize(viewport->WorkSize);
+        ImGui::SetNextWindowViewport(viewport->ID);
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+        window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+        window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
     }
-    ImGui::End(); // Main Window
+    else
+    {
+        dockspace_flags &= ~ImGuiDockNodeFlags_PassthruCentralNode;
+    }
+
+    // When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render our background
+    // and handle the pass-thru hole, so we ask Begin() to not render a background.
+    if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
+        window_flags |= ImGuiWindowFlags_NoBackground;
+
+    // Important: note that we proceed even if Begin() returns false (aka window is collapsed).
+    // This is because we want to keep our DockSpace() active. If a DockSpace() is inactive,
+    // all active windows docked into it will lose their parent and become undocked.
+    // We cannot preserve the docking relationship between an active window and an inactive docking, otherwise
+    // any change of dockspace/settings would lead to windows being stuck in limbo and never being visible.
+    if (!opt_padding)
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+    ImGui::Begin("DockSpace Demo", &dockspaceOpen, window_flags);
+    if (!opt_padding)
+        ImGui::PopStyleVar();
+
+    if (opt_fullscreen)
+        ImGui::PopStyleVar(2);
+
+    // DockSpace
+    if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
+    {
+        ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
+        ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+    }
+
+    if (ImGui::BeginMenuBar())
+    {
+        if (ImGui::BeginMenu("Options"))
+        {
+            // Disabling fullscreen would allow the window to be moved to the front of other windows,
+            // which we can't undo at the moment without finer window depth/z control.
+            ImGui::MenuItem("Fullscreen", NULL, &opt_fullscreen);
+            ImGui::MenuItem("Padding", NULL, &opt_padding);
+            ImGui::Separator();
+
+            if (ImGui::MenuItem("Flag: NoSplit", "", (dockspace_flags & ImGuiDockNodeFlags_NoSplit) != 0)) { dockspace_flags ^= ImGuiDockNodeFlags_NoSplit; }
+            if (ImGui::MenuItem("Flag: NoResize", "", (dockspace_flags & ImGuiDockNodeFlags_NoResize) != 0)) { dockspace_flags ^= ImGuiDockNodeFlags_NoResize; }
+            if (ImGui::MenuItem("Flag: NoDockingInCentralNode", "", (dockspace_flags & ImGuiDockNodeFlags_NoDockingInCentralNode) != 0)) { dockspace_flags ^= ImGuiDockNodeFlags_NoDockingInCentralNode; }
+            if (ImGui::MenuItem("Flag: AutoHideTabBar", "", (dockspace_flags & ImGuiDockNodeFlags_AutoHideTabBar) != 0)) { dockspace_flags ^= ImGuiDockNodeFlags_AutoHideTabBar; }
+            if (ImGui::MenuItem("Flag: PassthruCentralNode", "", (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode) != 0, opt_fullscreen)) { dockspace_flags ^= ImGuiDockNodeFlags_PassthruCentralNode; }
+            ImGui::Separator();
+
+            if (ImGui::MenuItem("Close", NULL, false))
+                dockspaceOpen = false;
+            ImGui::EndMenu();
+        }
+
+        ImGui::EndMenuBar();
+    }
+
+    if (ImGui::Begin("Renderer"))
+    {
+        ImVec2 wsize = ImGui::GetWindowSize();
+        ImGui::Image((ImTextureID)_fboViewport, wsize, ImVec2(0, 1), ImVec2(1, 0));
+    }
+    ImGui::End();
+
+    // Settings
+    ImGui::SetNextWindowSize(ImVec2(_viewportWidth, _viewportHeight), ImGuiCond_FirstUseEver);
+    if (ImGui::Begin("Procedural Planets Settings"))
+    {
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        ImGui::Checkbox("Wireframe Mode", &_wireframeMode);
+        ImGui::SliderInt("Resolution", &_resolution, 4, 128);
+        ImGui::ColorEdit3("Planet Color", (float*)&_planetColor);
+        ImGui::SliderFloat("Size", &_planetRadius, 0.2f, 4.0f);
+        ImGui::SliderFloat("Strength", &_noiseStrength, 0.0f, 2.0f);
+        ImGui::SliderFloat("Roughness", &_noiseRoughness, 0.0f, 20.0f);
+        ImGui::SliderFloat3("Center", (float*)&_noiseCenter, -10.0f, 10.0f);
+    }
+    ImGui::End(); // Settings
+
+
+    ImGui::End();
+
+
+
 
     // Render ImGUI
     ImGui::Render();
