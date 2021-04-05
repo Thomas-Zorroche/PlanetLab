70e15092c5173bf2c7756603d7dad59c792b3050
Thomas-Zorroche <thomas.zorroche@gmail.com>
2021-04-05 19:09:14 +0200

    [noies] add  multiples noises

--- src/noise/NoiseFilter.cpp
+++ src/noise/NoiseFilter.cpp
@@ -11,7 +11,21 @@ NoiseFilter::NoiseFilter(const std::shared_ptr<NoiseSettings>& settings, int see
 
 float NoiseFilter::evaluate(const glm::vec3& point) const
 {
-	glm::vec3 coordNoise = point * _settings->roughness() + _settings->center();
-	float noiseValue = (_noise.noise3D(coordNoise.x, coordNoise.y, coordNoise.z) + 1) * 0.5;
+	
+	//float noiseValue = (_noise.noise3D(coordNoise.x, coordNoise.y, coordNoise.z) + 1) * 0.5;
+	float noiseValue = 0;
+	float frequency = _settings->baseRoughness();
+	float amplitude = 1;
+
+	for (size_t i = 0; i < _settings->layerCount(); i++)
+	{
+		glm::vec3 coordNoise = point * frequency + _settings->center();
+		float v = _noise.noise3D(coordNoise.x, coordNoise.y, coordNoise.z);
+		noiseValue += (v + 1) * 0.5f * amplitude;
+
+		frequency *= _settings->roughness();
+		amplitude *= _settings->persistence();
+	}
+	
 	return noiseValue * _settings->strength();
 }
--- src/noise/NoiseSettings.hpp
+++ src/noise/NoiseSettings.hpp
@@ -8,15 +8,25 @@ public:
 	NoiseSettings() {};
 
 	float strength() const { return _strength; }
-	float roughness() const { return _roughness; }
+	float baseRoughness() const { return _roughness; }
+	float roughness() const { return _baseRoughness; }
 	glm::vec3 center() const { return _center; }
+	int layerCount() const { return _layersCount; }
+	float persistence() const { return _persistence; }
 
 	float& strength() { return _strength; }
+	float& baseRoughness() { return _roughness; }
 	float& roughness() { return _roughness; }
 	glm::vec3& center() { return _center; }
+	int& layerCount() { return _layersCount; }
+	float& persistence() { return _persistence; }
 
 private:
 	float _strength = 1.0f;
-	float _roughness = 1.0f;
+	float _baseRoughness = 1.0f;
+	float _roughness = 2.0f;
 	glm::vec3 _center = glm::vec3(0, 0, 0);
+
+	int _layersCount = 1;
+	float _persistence = 0.5f; // Amplitude will be half with each layer
 };
