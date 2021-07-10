cccb37dafcfc8cc8ba40d05e103cf5866f7914c6
Thomas Zorroche <thomas.zorroche@gmail.com>
2021-07-10 15:40:28 +0200

    [event] add ImGui callback for erase inside InputText

--- PlanetLab/src/event/InputHandler.cpp
+++ PlanetLab/src/event/InputHandler.cpp
@@ -81,6 +81,8 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 */
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
+    ImGui_ImplGlfw_KeyCallback(window, key, scancode, action, mods);
+
     if (key == (int)KeyCode::S && action == GLFW_PRESS)
     {
         if (Input::IsKeyPressed(KeyCode::CTRL))
